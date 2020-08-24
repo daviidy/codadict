14ac1a64664d1e78503cd232335bf148030f6758
aouman <37551703+aouman@users.noreply.github.com>
2020-08-24 16:33:09 +0000

    myproject box-shadow

--- resources/views/users/user_default/myProject.blade.php
+++ resources/views/users/user_default/myProject.blade.php
@@ -2,15 +2,20 @@
 @section('title', 'Mes projets')
 
 @section('content')
+<style media="screen">
+  .hover:hover{
+    box-shadow: inset 0px 0px 1px 0 rgba(0, 0, 0, 0.2), 0px 2px 5px 0 rgba(0, 0, 0, 0.19);
 
+  }
+</style>
             <!-- MAIN CONTENT-->
             <div class="main-content">
                 <div class="section__content section__content--p30">
                     <div class="container-fluid">
                         <div class="row">
-
+                          <a href="#">
                             <div class="col-md-4">
-                                <div class="card">
+                                <div class="card hover" style="">
                                     <img class="card-img-top" src="/assets/imgs/bg-title-01.jpg" alt="Card image cap">
                                     <div class="card-body">
                                         <h4 class="card-title mb-3">Card Image Title</h4>
@@ -20,6 +25,7 @@
                                     </div>
                                 </div>
                             </div>
+                          </a>
                         </div>
                     </div>
                 </div>
