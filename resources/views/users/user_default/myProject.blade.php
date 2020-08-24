25ea097a30317413e3b9268108d4fe2c51ec1811
aouman <37551703+aouman@users.noreply.github.com>
2020-08-24 16:12:25 +0000

    default dashboard

---
+++ resources/views/users/user_default/myProject.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.menu_user_default')
+@section('title', 'Mes projets')
+
+@section('content')
+
+            <!-- MAIN CONTENT-->
+            <div class="main-content">
+                <div class="section__content section__content--p30">
+                    <div class="container-fluid">
+                        <div class="row">
+
+                            <div class="col-md-4">
+                                <div class="card">
+                                    <img class="card-img-top" src="/assets/imgs/bg-title-01.jpg" alt="Card image cap">
+                                    <div class="card-body">
+                                        <h4 class="card-title mb-3">Card Image Title</h4>
+                                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+                                            content.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- END PAGE CONTAINER-->
+
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,23 @@ Route::get('/', function () {
 Route::get('/codact', function () {
     return view('codact');
 });
+
+Route::get('/users/default', function () {
+    return view('/users/user_default/index');
+});
+
+Route::get('/users/myproject', function () {
+    return view('/users/user_default/myProject');
+});
+
+Route::get('/users/createproject', function () {
+    return view('/users/user_default/creat_project');
+});
+
+Route::get('/users/editproject', function () {
+    return view('/users/user_default/edit_project');
+});
+
+Route::get('/users/setting', function () {
+    return view('/users/user_default/setting_user');
+});
